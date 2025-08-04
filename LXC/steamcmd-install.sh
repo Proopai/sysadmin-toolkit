47ac52b7cccfafc49f693f3d9c2f807853848e71
Brad Charboneau <4032470+brad-charboneau@users.noreply.github.com>
2025-08-04 09:03:05 -0500

    Create steamcmd-install.sh

---
+++ LXC/steamcmd-install.sh
@@ -0,0 +1,21 @@
+# Update Base Apt Packages
+apt update
+apt upgrade
+# Steamcmd Pre-Reqs
+apt install software-properties-common
+apt-add-repository non-free
+dpkg --add-architecture i386
+apt update
+# Install Steamcmd
+apt install steamcmd
+
+# Make steamcmd executable from anywhere
+ln -s /usr/games/steamcmd /usr/bin
+
+# Reconfigure Locals to include en_US.UTF8 UTF8 for proper operation of steamcmd
+echo "locales locales/default_environment_locale select en_US.UTF-8 UTF-8" | debconf-set-selections
+echo "locales locales/locales_to_be_generated multiselect en_US.UTF-8 UTF-8" | debconf-set-selections
+DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
+
+# Execute SteamCMD to update it to the latest version
+steamcmd
